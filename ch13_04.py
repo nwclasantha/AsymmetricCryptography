8732619c313bd5969923d6d1203d651a288db5e1
Chanaka Lasantha <48653609+nwclasantha@users.noreply.github.com>
2021-05-20 14:06:18 +0530

    Add files via upload

---
+++ ch13_04.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+# generate private and public keys
+# Then, save them into files
+print('generating private and public keys...')
+key = RSA.generate(2048)
+f = open('my_rsa_private_key.pem', 'wb')
+f.write(key.exportKey('PEM'))
+f.close()
+f = open('my_rsa_public_key.pem', 'wb')
+f.write(key.publickey().exportKey('PEM'))
+f.close()
+print('done')
+
+
+message = 'hello world from python'
+print('plaintext:', message)
+
+# encrypt data using public key
+f = open('my_rsa_public_key.pem','r')
+RSAkey = RSA.importKey(f.read())
+f.close()
+k = Random.new().read(8)
+encrypted_msg = RSAkey.encrypt(message.encode(), k)
+ciphertext = encrypted_msg[0]
+print('encrypted:', ciphertext)
+
+
+
